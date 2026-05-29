25a5c54182a9a9d97a91e10ee836fb475b4794b5
Md Zeeshan Uddin <zeeshancloud15@gmail.com>
2026-05-29 18:14:52 +0530

    Update app.js

--- user-service/app.js
+++ user-service/app.js
@@ -2,17 +2,12 @@ const express = require('express')
 
 const app = express()
 
-app.get('/users', (req, res) => {
+app.get('/support', (req, res) => {
 
-    res.send('User Service Running')
-})
-
-app.get('/register', (req, res) => {
-
-    res.send('Register API Running')
+    res.send('Support Service Running')
 })
 
 app.listen(3000, () => {
 
-    console.log('User Service Started')
+    console.log('Support Service Started')
 })
