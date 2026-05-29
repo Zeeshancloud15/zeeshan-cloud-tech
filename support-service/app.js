2098bdbe8baab0da4890e96ea6af1e71632c8605
Md Zeeshan Uddin <zeeshancloud15@gmail.com>
2026-05-29 18:16:55 +0530

    Update and rename server.js to app.js

---
+++ support-service/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/support', (req, res) => {
+
+    res.send('Support Service Running')
+})
+
+app.listen(3000, () => {
+
+    console.log('Support Service Started')
+})
